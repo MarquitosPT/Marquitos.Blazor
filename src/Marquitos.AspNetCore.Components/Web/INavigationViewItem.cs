0973753e0ce624f689a706ffbe7168cc42b7d0d0
Marcos Gomes <marquitospt@hotmail.com>
2021-08-27 19:14:25 +0100

    Edited Index page

--- src/Marquitos.AspNetCore.Components/Web/INavigationViewItem.cs
+++ src/Marquitos.AspNetCore.Components/Web/INavigationViewItem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 
 namespace Marquitos.AspNetCore.Components.Web
 {
@@ -10,5 +11,7 @@ namespace Marquitos.AspNetCore.Components.Web
     {
         string Uri { get; }
         public bool Enabled { get; set; }
+        RenderFragment ChildContent { get; }
+        RenderFragment Content { get; }
     }
 }
