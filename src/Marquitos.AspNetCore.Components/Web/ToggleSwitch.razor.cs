8db19369293975df1d6c8b9a15ca23ce1e352e6a
Marcos Gomes <marquitospt@hotmail.com>
2022-08-06 16:20:17 +0000

    Added ToggleSwitch component

--- src/Marquitos.AspNetCore.Components/Web/ToggleSwitch.razor.cs
+++ src/Marquitos.AspNetCore.Components/Web/ToggleSwitch.razor.cs
@@ -1,24 +1,54 @@
-﻿using System;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace Marquitos.AspNetCore.Components.Web
 {
-	public partial class ToggleSwitch : Component
+    public partial class ToggleSwitch : Component
 	{
-        [Parameter]
-        public string Header { get; set; }
+        private bool _isOn = false;
 
         [Parameter]
-        public string OnContent { get; set; }
+        public string OnContent { get; set; } = "On";
 
         [Parameter]
-        public string OffContent { get; set; }
+        public string OffContent { get; set; } = "Off";
 
         [Parameter]
-        public bool IsOn { get; set; }
+        public bool IsOn 
+        { 
+            get { return _isOn; }
+            set
+            {
+                if (_isOn != value)
+                {
+                    _isOn = value;
+
+                    if (Toggled.HasDelegate)
+                    {
+                        Toggled.InvokeAsync();
+                    }
+                }
+            }
+        }
+
+        [Parameter]
+        public bool Enabled { get; set; } = true;
 
         [Parameter]
         public EventCallback Toggled { get; set; }
+
+        private void CheckChange(ChangeEventArgs args)
+        {
+            IsOn = (bool)args.Value;
+        }
+
+        private void LabelClick(MouseEventArgs args)
+        {
+            if (Enabled)
+            {
+                IsOn = !IsOn;
+            }
+        }
     }
 }
 
