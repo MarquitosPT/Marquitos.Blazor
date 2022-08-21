7248dc27827c1efb1072570afa2dfb96521b7c60
Marcos Gomes <marquitospt@hotmail.com>
2022-08-21 12:09:18 +0000

    Updated ToggleSwitch

--- src/Marquitos.AspNetCore.Components/Web/ToggleSwitch.razor.cs
+++ src/Marquitos.AspNetCore.Components/Web/ToggleSwitch.razor.cs
@@ -3,16 +3,28 @@ using Microsoft.AspNetCore.Components.Web;
 
 namespace Marquitos.AspNetCore.Components.Web
 {
+    /// <summary>
+    /// Toggle switch component
+    /// </summary>
     public partial class ToggleSwitch : Component
 	{
         private bool _isOn = false;
 
+        /// <summary>
+        /// The content to display when the switch is on.
+        /// </summary>
         [Parameter]
         public string OnContent { get; set; } = "On";
 
+        /// <summary>
+        /// The content to display when the switch is off.
+        /// </summary>
         [Parameter]
         public string OffContent { get; set; } = "Off";
 
+        /// <summary>
+        /// Gets or sets whether the switch is on or off.
+        /// </summary>
         [Parameter]
         public bool IsOn 
         { 
@@ -25,17 +37,35 @@ namespace Marquitos.AspNetCore.Components.Web
 
                     if (Toggled.HasDelegate)
                     {
-                        Toggled.InvokeAsync();
+                        Toggled.InvokeAsync().Wait();
                     }
                 }
             }
         }
 
+        /// <summary>
+        /// Gets or sets whether the switch is enabled or disabled. 
+        /// When disabled the switch becomes read only.
+        /// </summary>
         [Parameter]
         public bool Enabled { get; set; } = true;
 
+        /// <summary>
+        /// The event that is fired when the switch is turned on or turned off.
+        /// </summary>
         [Parameter]
         public EventCallback Toggled { get; set; }
+
+        /// <summary>
+        /// Toggles the switch state between on or off.
+        /// </summary>
+        public void Toggle()
+        {
+            if (Enabled)
+            {
+                IsOn = !IsOn;
+            }
+        }
 
         private void CheckChange(ChangeEventArgs args)
         {
@@ -44,10 +74,7 @@ namespace Marquitos.AspNetCore.Components.Web
 
         private void LabelClick(MouseEventArgs args)
         {
-            if (Enabled)
-            {
-                IsOn = !IsOn;
-            }
+            Toggle();
         }
     }
 }
